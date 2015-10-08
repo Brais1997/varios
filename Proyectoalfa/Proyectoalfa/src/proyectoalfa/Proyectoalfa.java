70cbd7443f5d1dfacec4abe65c1ec17200ef11f8
amantillapousa <amantillapousa@atanasoff32.danielcastelao.org>
2015-10-08 09:09:50 +0200

    version 0.1 Programa sin terminar. Solo permite intruducir el valor "ºC" a convertir.

---
+++ Proyectoalfa/Proyectoalfa/src/proyectoalfa/Proyectoalfa.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoalfa;
+import java.util.Scanner;
+
+/**
+ * @author amantillapousa
+ * 1º ejercicio para trabajar con Git.
+ * Calculadora temperatura. 
+ */
+public class Proyectoalfa {
+
+
+    public static void main(String[] args) {
+       
+        int centigrados;
+        int resultado;
+        double fahr;
+        fahr = 33.8;
+        double kelv;
+        kelv = 274.15;
+        
+        Scanner numero= new Scanner (System.in);
+        
+        System.out.print("Ingrese el valor de ºC: ");
+        centigrados = numero.nextInt();
+        
+        System.out.print(System.getProperty("line.separator"));
+        
+        
+    
+    }
+    
+}
